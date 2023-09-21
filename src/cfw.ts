3060165f851ac0d16dc65f0aa71a845ca6df4f6d
Jason Caldwell <jaswrks@o5p.me>
2023-09-21 03:29:42 -0400

    Project update. [p][robotic]

--- src/cfw.ts
+++ src/cfw.ts
@@ -7,10 +7,10 @@ import './resources/init-env.ts';
 import type { $type } from '@clevercanyon/utilities';
 import { $env, $http, $json, $str, $url } from '@clevercanyon/utilities';
 import {
-	MethodNotAllowedError as cfKVAꓺMethodNotAllowedError,
-	NotFoundError as cfKVAꓺNotFoundError,
-	getAssetFromKV as cfKVAꓺgetAssetFromKV,
-	mapRequestToAsset as cfKVAꓺmapRequestToAsset,
+    MethodNotAllowedError as cfKVAꓺMethodNotAllowedError,
+    NotFoundError as cfKVAꓺNotFoundError,
+    getAssetFromKV as cfKVAꓺgetAssetFromKV,
+    mapRequestToAsset as cfKVAꓺmapRequestToAsset,
 } from '@cloudflare/kv-asset-handler';
 
 const cache = (caches as unknown as $type.cf.CacheStorage).default;
@@ -21,32 +21,32 @@ const cache = (caches as unknown as $type.cf.CacheStorage).default;
 export type Context = $type.cf.ExecutionContext;
 
 export type Environment = {
-	readonly D1?: $type.cf.D1Database;
-	readonly R2?: $type.cf.R2Bucket;
-	readonly KV?: $type.cf.KVNamespace;
-	readonly DO?: $type.cf.DurableObjectNamespace;
-	readonly __STATIC_CONTENT?: $type.cf.KVNamespace;
-	readonly [x: string]: unknown;
+    readonly D1?: $type.cf.D1Database;
+    readonly R2?: $type.cf.R2Bucket;
+    readonly KV?: $type.cf.KVNamespace;
+    readonly DO?: $type.cf.DurableObjectNamespace;
+    readonly __STATIC_CONTENT?: $type.cf.KVNamespace;
+    readonly [x: string]: unknown;
 };
 export type Route = (x: FetchEventData) => Promise<$type.cf.Response>;
 
 export type Routes = {
-	readonly subpathGlobs: {
-		readonly [x: string]: Route;
-	};
+    readonly subpathGlobs: {
+        readonly [x: string]: Route;
+    };
 };
 export type FetchEventData = {
-	readonly request: $type.cf.Request;
-	readonly env: Environment;
-	readonly ctx: Context;
-	readonly routes: Routes;
-	readonly url: $type.cf.URL;
+    readonly request: $type.cf.Request;
+    readonly env: Environment;
+    readonly ctx: Context;
+    readonly routes: Routes;
+    readonly url: $type.cf.URL;
 };
 export type InitialFetchEventData = {
-	readonly request: $type.cf.Request;
-	readonly env: Environment;
-	readonly ctx: Context;
-	readonly routes: Routes;
+    readonly request: $type.cf.Request;
+    readonly env: Environment;
+    readonly ctx: Context;
+    readonly routes: Routes;
 };
 
 /**
@@ -57,31 +57,31 @@ export type InitialFetchEventData = {
  * @returns        Response promise.
  */
 export const handleFetchEvent = async (ifeData: InitialFetchEventData): Promise<$type.cf.Response> => {
-	let { request } = ifeData;
-	const { env, ctx, routes } = ifeData;
+    let { request } = ifeData;
+    const { env, ctx, routes } = ifeData;
 
-	$env.capture('@global', env); // Captures environment vars.
-	const appBasePath = String($env.get('@top', 'APP_BASE_PATH', ''));
+    $env.capture('@global', env); // Captures environment vars.
+    const appBasePath = String($env.get('@top', 'APP_BASE_PATH', ''));
 
-	try {
-		request = $http.prepareRequest(request, {}) as $type.cf.Request;
-		const url = $url.parse(request.url) as $type.cf.URL;
-		const feData = { request, env, ctx, routes, url };
-		if (
-			$http.requestPathIsStatic(request, url) && //
-			$env.get('@top', '__STATIC_CONTENT' /* Worker site? */) &&
-			$str.matches(url.pathname, appBasePath + '/assets/**')
-		) {
-			return handleFetchCache(handleFetchStaticAssets, feData);
-		}
-		return handleFetchCache(handleFetchDynamics, feData);
-		//
-	} catch (error) {
-		if (error instanceof Response) {
-			return error as unknown as $type.cf.Response;
-		}
-		return $http.prepareResponse(request, { status: 500 }) as $type.cf.Response;
-	}
+    try {
+        request = $http.prepareRequest(request, {}) as $type.cf.Request;
+        const url = $url.parse(request.url) as $type.cf.URL;
+        const feData = { request, env, ctx, routes, url };
+        if (
+            $http.requestPathIsStatic(request, url) && //
+            $env.get('@top', '__STATIC_CONTENT' /* Worker site? */) &&
+            $str.matches(url.pathname, appBasePath + '/assets/**')
+        ) {
+            return handleFetchCache(handleFetchStaticAssets, feData);
+        }
+        return handleFetchCache(handleFetchDynamics, feData);
+        //
+    } catch (error) {
+        if (error instanceof Response) {
+            return error as unknown as $type.cf.Response;
+        }
+        return $http.prepareResponse(request, { status: 500 }) as $type.cf.Response;
+    }
 };
 
 /**
@@ -93,28 +93,28 @@ export const handleFetchEvent = async (ifeData: InitialFetchEventData): Promise<
  * @returns        Response promise.
  */
 export const handleFetchCache = async (route: Route, feData: FetchEventData): Promise<$type.cf.Response> => {
-	const { request, ctx } = feData;
-	let cachedResponse; // Initialize.
+    const { request, ctx } = feData;
+    let cachedResponse; // Initialize.
 
-	if (!$http.requestHasCacheableMethod(request)) {
-		return route(feData); // Not applicable.
-	}
-	if ((cachedResponse = await cache.match(request, { ignoreMethod: true }))) {
-		if (!$http.requestNeedsContentBody(request, cachedResponse.status)) {
-			cachedResponse = new Response(null /* No response body. */, {
-				status: cachedResponse.status,
-				statusText: cachedResponse.statusText,
-				headers: cachedResponse.headers,
-			}) as unknown as $type.cf.Response;
-		}
-		return cachedResponse;
-	}
-	const response = await route(feData);
+    if (!$http.requestHasCacheableMethod(request)) {
+        return route(feData); // Not applicable.
+    }
+    if ((cachedResponse = await cache.match(request, { ignoreMethod: true }))) {
+        if (!$http.requestNeedsContentBody(request, cachedResponse.status)) {
+            cachedResponse = new Response(null /* No response body. */, {
+                status: cachedResponse.status,
+                statusText: cachedResponse.statusText,
+                headers: cachedResponse.headers,
+            }) as unknown as $type.cf.Response;
+        }
+        return cachedResponse;
+    }
+    const response = await route(feData);
 
-	if ('GET' === request.method && 206 !== response.status && '*' !== response.headers.get('vary')) {
-		ctx.waitUntil(cache.put(request, response.clone()));
-	}
-	return response;
+    if ('GET' === request.method && 206 !== response.status && '*' !== response.headers.get('vary')) {
+        ctx.waitUntil(cache.put(request, response.clone()));
+    }
+    return response;
 };
 
 /**
@@ -125,15 +125,15 @@ export const handleFetchCache = async (route: Route, feData: FetchEventData): Pr
  * @returns        Response promise.
  */
 export const handleFetchDynamics = async (feData: FetchEventData): Promise<$type.cf.Response> => {
-	const { request, routes, url } = feData;
-	const appBasePath = String($env.get('@top', 'APP_BASE_PATH', ''));
+    const { request, routes, url } = feData;
+    const appBasePath = String($env.get('@top', 'APP_BASE_PATH', ''));
 
-	for (const [routeSubpathGlob, routeSubpathHandler] of Object.entries(routes.subpathGlobs)) {
-		if ($str.matches(url.pathname, appBasePath + '/' + routeSubpathGlob)) {
-			return routeSubpathHandler(feData);
-		}
-	}
-	return $http.prepareResponse(request, { status: 404 }) as $type.cf.Response;
+    for (const [routeSubpathGlob, routeSubpathHandler] of Object.entries(routes.subpathGlobs)) {
+        if ($str.matches(url.pathname, appBasePath + '/' + routeSubpathGlob)) {
+            return routeSubpathHandler(feData);
+        }
+    }
+    return $http.prepareResponse(request, { status: 404 }) as $type.cf.Response;
 };
 
 /**
@@ -144,44 +144,44 @@ export const handleFetchDynamics = async (feData: FetchEventData): Promise<$type
  * @returns        Response promise.
  */
 export const handleFetchStaticAssets = async (feData: FetchEventData): Promise<$type.cf.Response> => {
-	const { request, ctx } = feData;
-	const appBasePath = String($env.get('@top', 'APP_BASE_PATH', ''));
+    const { request, ctx } = feData;
+    const appBasePath = String($env.get('@top', 'APP_BASE_PATH', ''));
 
-	try {
-		const kvAssetEventData = {
-			request: request as unknown as Request,
-			waitUntil(promise: Promise<void>): void {
-				ctx.waitUntil(promise);
-			},
-		};
-		const response = await cfKVAꓺgetAssetFromKV(kvAssetEventData, {
-			ASSET_NAMESPACE: $env.get('@top', '__STATIC_CONTENT') as string,
-			// @ts-ignore: This is dynamically resolved by Cloudflare.
-			// eslint-disable-next-line @typescript-eslint/no-unsafe-argument -- manifest ok.
-			ASSET_MANIFEST: $json.parse(await import('__STATIC_CONTENT_MANIFEST')) as { [x: string]: string },
+    try {
+        const kvAssetEventData = {
+            request: request as unknown as Request,
+            waitUntil(promise: Promise<void>): void {
+                ctx.waitUntil(promise);
+            },
+        };
+        const response = await cfKVAꓺgetAssetFromKV(kvAssetEventData, {
+            ASSET_NAMESPACE: $env.get('@top', '__STATIC_CONTENT') as string,
+            // @ts-ignore: This is dynamically resolved by Cloudflare.
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-argument -- manifest ok.
+            ASSET_MANIFEST: $json.parse(await import('__STATIC_CONTENT_MANIFEST')) as { [x: string]: string },
 
-			defaultDocument: 'index.html',
-			defaultMimeType: 'application/octet-stream',
-			cacheControl: { edgeTTL: 31536000, browserTTL: 31536000 },
+            defaultDocument: 'index.html',
+            defaultMimeType: 'application/octet-stream',
+            cacheControl: { edgeTTL: 31536000, browserTTL: 31536000 },
 
-			mapRequestToAsset: (request: Request): Request => {
-				const url = new URL(request.url); // URL is rewritten below.
+            mapRequestToAsset: (request: Request): Request => {
+                const url = new URL(request.url); // URL is rewritten below.
 
-				const regExp = new RegExp('^' + $str.escRegExp(appBasePath + '/assets/'), 'u');
-				url.pathname = url.pathname.replace(regExp, '/'); // Removes `/assets` prefix.
+                const regExp = new RegExp('^' + $str.escRegExp(appBasePath + '/assets/'), 'u');
+                url.pathname = url.pathname.replace(regExp, '/'); // Removes `/assets` prefix.
 
-				return cfKVAꓺmapRequestToAsset(new Request(url, request));
-			},
-		});
-		return $http.prepareResponse(request, { ...response }) as $type.cf.Response;
-		//
-	} catch (error) {
-		if (error instanceof cfKVAꓺNotFoundError) {
-			return $http.prepareResponse(request, { status: 404 }) as $type.cf.Response;
-		}
-		if (error instanceof cfKVAꓺMethodNotAllowedError) {
-			return $http.prepareResponse(request, { status: 405 }) as $type.cf.Response;
-		}
-		return $http.prepareResponse(request, { status: 500 }) as $type.cf.Response;
-	}
+                return cfKVAꓺmapRequestToAsset(new Request(url, request));
+            },
+        });
+        return $http.prepareResponse(request, { ...response }) as $type.cf.Response;
+        //
+    } catch (error) {
+        if (error instanceof cfKVAꓺNotFoundError) {
+            return $http.prepareResponse(request, { status: 404 }) as $type.cf.Response;
+        }
+        if (error instanceof cfKVAꓺMethodNotAllowedError) {
+            return $http.prepareResponse(request, { status: 405 }) as $type.cf.Response;
+        }
+        return $http.prepareResponse(request, { status: 500 }) as $type.cf.Response;
+    }
 };
--- src/geo.ts
+++ src/geo.ts
@@ -15,7 +15,7 @@ import type { FetchEventData as $cfwꓺFetchEventData } from './cfw.ts';
  * @returns     Geo property value.
  */
 export const prop = (fed: $cfwꓺFetchEventData, prop: string): string => {
-	const { request } = fed; // Request extraction.
-	const r = request as unknown as $type.cf.Request; // Includes `cf` property.
-	return String(r.cf && prop in r.cf ? r.cf[prop as keyof typeof r.cf] || '' : '');
+    const { request } = fed; // Request extraction.
+    const r = request as unknown as $type.cf.Request; // Includes `cf` property.
+    return String(r.cf && prop in r.cf ? r.cf[prop as keyof typeof r.cf] || '' : '');
 };
