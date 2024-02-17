4eb10fddc1419ebfbcfe141c456b604f65d2ea75
Jason Caldwell <jaswrks@o5p.me>
2024-02-17 04:36:46 -0500

    Project update. [p][robotic]

--- src/proxy.ts
+++ src/proxy.ts
@@ -5,22 +5,49 @@
 import '#@initialize.ts';
 
 import { $cfw, cfw } from '#index.ts';
-import { $crypto, $http, $mime, $obj, $str, $time, $url, type $type } from '@clevercanyon/utilities';
+import { $crypto, $env, $http, $is, $mime, $obj, $str, $time, $url, type $type } from '@clevercanyon/utilities';
 
 /**
  * Defines types.
  */
 export type FetchOptions = {
-    proxy: {
-        host: string;
-        port: number;
+    proxy?: {
+        host?: string;
+        port?: number;
         username?: string;
         password?: string;
     };
     method?: 'HEAD' | 'GET';
-    headers?: $type.cfw.Headers | { [x: string]: string };
+    headers?: $type.cfw.HeadersInit;
+
+    maxRedirects?: number;
     timeout?: number; // In milliseconds.
 };
+type RequiredFetchOptions = Required<FetchOptions> & {
+    proxy: Required<FetchOptions['proxy']>;
+    headers: $type.cfw.Headers;
+};
+type FakeUAHeaders = $type.ReadonlyDeep<{
+    'user-agent': string;
+
+    'accept': string;
+    'accept-encoding': string;
+    'accept-language': string;
+
+    'sec-ch-ua': string;
+    'sec-ch-ua-mobile': string;
+    'sec-ch-ua-platform': string;
+    'sec-fetch-site': string;
+    'sec-fetch-mod': string;
+    'sec-fetch-user': string;
+
+    'upgrade-insecure-requests': string;
+}>;
+type FakeUAHeadersResponsePayload = $type.ReadonlyDeep<{
+    ok: boolean;
+    error?: { message: string };
+    data?: FakeUAHeaders; // When `ok` is true.
+}>;
 
 /**
  * Performs an HTTP fetch using a proxy.
@@ -37,21 +64,31 @@ export const fetch = async (rcData: $cfw.StdRequestContextData, parseable: $type
     const { Response } = cfw,
         url = $url.tryParse(parseable),
         opts = $obj.defaults({}, options || {}, {
+            proxy: {
+                host: $env.get('SSR_APP_ROTATING_PROXY_HOST', { type: 'string' }) || $env.get('APP_ROTATING_PROXY_HOST', { type: 'string' }),
+                port: $env.get('SSR_APP_ROTATING_PROXY_PORT', { type: 'number' }) || $env.get('APP_ROTATING_PROXY_PORT', { type: 'number' }) || 0,
+
+                username: $env.get('SSR_APP_ROTATING_PROXY_USERNAME', { type: 'string' }) || $env.get('APP_ROTATING_PROXY_USERNAME', { type: 'string' }),
+                password: $env.get('SSR_APP_ROTATING_PROXY_PASSWORD', { type: 'string' }) || $env.get('APP_ROTATING_PROXY_PASSWORD', { type: 'string' }),
+            },
             method: 'GET',
             headers: {},
-            timeout: $time.secondInMilliseconds * 15,
-        }) as Required<FetchOptions>;
 
-    if (!url) {
+            maxRedirects: 2,
+            timeout: $time.secondInMilliseconds * 15,
+        }) as RequiredFetchOptions;
+
+    opts.headers = $http.parseHeaders(opts.headers) as $type.cfw.Headers;
+    if (!opts.headers.has('user-agent'))
+        for (const [name, value] of Object.entries(await fetchꓺfakeUAHeaders(rcData))) {
+            opts.headers.set(name, value);
+        }
+    if (!url || !opts.proxy?.host || !opts.proxy?.port || !opts.method || !opts.timeout)
         return new Response(null, {
             status: 400,
             statusText: $http.responseStatusText(400),
             headers: { 'content-type': $mime.contentType('.txt') },
         });
-    }
-    if (!opts.method || !opts.proxy || !opts.proxy.host || !opts.proxy.port || !opts.timeout) {
-        throw Error('AzwqQc85'); // Missing required options.
-    }
     return await Promise.race([fetchꓺwaitTimeout(rcData, opts), fetchꓺviaSocket(rcData, url, opts)]);
 };
 
@@ -59,11 +96,11 @@ export const fetch = async (rcData: $cfw.StdRequestContextData, parseable: $type
  * Creates a timeout promise.
  *
  * @param   rcData  Request context data; {@see $cfw.StdRequestContextData}.
- * @param   options Required options; {@see Required<FetchOptions>}.
+ * @param   options Required options; {@see RequiredFetchOptions}.
  *
  * @returns         Timeout promise suitable for a race.
  */
-const fetchꓺwaitTimeout = async (rcData: $cfw.StdRequestContextData, opts: Required<FetchOptions>): Promise<$type.cfw.Response> => {
+const fetchꓺwaitTimeout = async (rcData: $cfw.StdRequestContextData, opts: RequiredFetchOptions): Promise<$type.cfw.Response> => {
     const { Response } = cfw;
 
     return new Promise((resolve): void => {
@@ -82,20 +119,25 @@ const fetchꓺwaitTimeout = async (rcData: $cfw.StdRequestContextData, opts: Req
 /**
  * Performs an HTTP fetch using a proxy.
  *
- * @param   rcData  Request context data; {@see $cfw.StdRequestContextData}.
- * @param   url     Parseable URL; i.e., string or URL instance.
- * @param   options Required options; {@see Required<FetchOptions>}.
+ * @param   rcData    Request context data; {@see $cfw.StdRequestContextData}.
+ * @param   url       Parseable URL; i.e., string or URL instance.
+ * @param   options   Required options; {@see RequiredFetchOptions}.
+ * @param   redirects Do not pass. Internal use only.
  *
- * @returns         Promise of HTTP response.
+ * @returns           Promise of HTTP response.
  */
-const fetchꓺviaSocket = async (rcData: $cfw.StdRequestContextData, url: $type.cfw.URL, opts: Required<FetchOptions>): Promise<$type.cfw.Response> => {
+const fetchꓺviaSocket = async (rcData: $cfw.StdRequestContextData, url: $type.cfw.URL, opts: RequiredFetchOptions, redirects: number = 0): Promise<$type.cfw.Response> => {
     const { Response } = cfw,
         sockets = await import('cloudflare:sockets');
 
     try {
-        const socket = sockets.connect({ hostname: opts.proxy.host, port: opts.proxy.port }),
+        const socket = sockets.connect({
+                hostname: opts.proxy.host,
+                port: opts.proxy.port,
+            }),
             { readable, writable } = socket,
             writer = writable.getWriter(),
+            //
             headers: Set<string> = new Set();
 
         headers.add(`host: ${url.hostname}`);
@@ -103,7 +145,7 @@ const fetchꓺviaSocket = async (rcData: $cfw.StdRequestContextData, url: $type.
         if (opts.proxy.username && opts.proxy.password) {
             headers.add(`proxy-authorization: ${'Basic ' + $crypto.base64Encode(`${opts.proxy.username}:${opts.proxy.password}`)}`);
         }
-        for (const [name, value] of $http.parseHeaders(opts.headers).entries()) {
+        for (const [name, value] of opts.headers.entries()) {
             headers.add(`${name}: ${value}`);
         }
         await writer.write(
@@ -114,15 +156,14 @@ const fetchꓺviaSocket = async (rcData: $cfw.StdRequestContextData, url: $type.
         );
         const rawHTTPResponse = await new Response(readable, { headers: { 'content-type': $mime.contentType('.txt') } }).text();
         await writer.close(); // Closes writable stream.
-        await socket.close(); // Closes socket.
+        await socket.close(); // Closes socket also.
 
-        if (!rawHTTPResponse /* Must at least contain headers. */) {
+        if (!rawHTTPResponse /* Must at least contain headers. */)
             return new Response(null, {
                 status: 421,
                 statusText: $http.responseStatusText(421),
                 headers: { 'content-type': $mime.contentType('.txt') },
             });
-        }
         const rawHTTPResponseCRLFIndex = rawHTTPResponse.indexOf('\r\n\r\n'), // Potentially `-1`; i.e., no response body.
             rawHTTPResponseHeaders = rawHTTPResponseCRLFIndex === -1 ? rawHTTPResponse : rawHTTPResponse.slice(0, rawHTTPResponseCRLFIndex).trim(),
             rawHTTPResponseBody = rawHTTPResponseCRLFIndex === -1 ? '' : rawHTTPResponse.slice(rawHTTPResponseCRLFIndex + 4).trim();
@@ -131,6 +172,10 @@ const fetchꓺviaSocket = async (rcData: $cfw.StdRequestContextData, url: $type.
             responseHeaders = $http.parseHeaders(rawHTTPResponseHeaders) as $type.cfw.Headers,
             responseBody = rawHTTPResponseBody;
 
+        if ([301, 302].includes(responseStatus) && responseHeaders.has('location') && redirects + 1 <= opts.maxRedirects) {
+            const redirectURL = $url.tryParse(responseHeaders.get('location') || '');
+            if (redirectURL) return fetchꓺviaSocket(rcData, redirectURL, opts, redirects + 1);
+        }
         return new Response(responseBody, {
             status: responseStatus,
             statusText: $http.responseStatusText(responseStatus),
@@ -143,4 +188,56 @@ const fetchꓺviaSocket = async (rcData: $cfw.StdRequestContextData, url: $type.
             headers: { 'content-type': $mime.contentType('.txt') },
         });
     }
+};
+
+/**
+ * Gets fake UA headers.
+ *
+ * @param   rcData Request context data; {@see $cfw.StdRequestContextData}.
+ *
+ * @returns        Promise of fake UA headers.
+ */
+const fetchꓺfakeUAHeaders = async (rcData: $cfw.StdRequestContextData): Promise<FakeUAHeaders> => {
+    const { fetch } = cfw,
+        { auditLogger } = rcData,
+        defaultUAHeaders = {
+            'upgrade-insecure-requests': '1',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'sec-ch-ua': 'Google Chrome;v="80", "Chromium";v="80", ";Not A Brand";v="99"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': 'Windows',
+            'sec-fetch-site': 'none',
+            'sec-fetch-mod': '',
+            'sec-fetch-user': '?1',
+            'accept-encoding': 'gzip, deflate',
+            'accept-language': 'en-US,en;q=0.9',
+        };
+    if (rcData.url.toString().startsWith('https://workers.hop.gdn/utilities/')) {
+        const { env: { KV: kv } } = rcData, // prettier-ignore
+            kvKey = 'fakeUAHeaders:' + String($crypto.randomNumber(1, 100)),
+            uaHeaders = (await kv.get(kvKey, { type: 'json' })) as FakeUAHeaders;
+
+        if (!$is.plainObject(uaHeaders)) {
+            const error = Error('q9UTub4N');
+            void auditLogger.warn('Fake UA headers failure.', { error });
+            return defaultUAHeaders;
+        }
+        return uaHeaders;
+    }
+    const payload = await fetch(
+        $url.addQueryVars(
+            { randomIndex: String($crypto.randomNumber(1, 100)) }, //
+            'https://workers.hop.gdn/utilities/api/fake-ua-headers/v1',
+        ),
+    )
+        .then((response) => response.json() as unknown as FakeUAHeadersResponsePayload)
+        .catch(() => ({ ok: false, error: { message: 'a8cvwGmQ' } }) as FakeUAHeadersResponsePayload);
+
+    if (!payload?.ok || !$is.plainObject(payload.data)) {
+        const error = Error('DkkbNUJr');
+        void auditLogger.warn('Fake UA headers failure.', { error });
+        return defaultUAHeaders;
+    }
+    return payload.data;
 };
