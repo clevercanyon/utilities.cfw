4e7d4df627cc821528c0926aaee096a00694bacf
Jason Caldwell <jaswrks@o5p.me>
2025-03-06 10:45:40 -0500

    Project update. [p][robotic]

--- dev/.files/vite/includes/vitest/config.mjs
+++ dev/.files/vite/includes/vitest/config.mjs
@@ -293,9 +293,16 @@ export default async ({ mode, projDir, srcDir, logsDir, pkg, targetEnv, vitestSa
                                     $concat: {
                                         include: [
                                             '@clevercanyon/utilities', //
+                                            '@clevercanyon/utilities/**',
+
                                             '@clevercanyon/utilities.web',
+                                            '@clevercanyon/utilities.web/**',
+
                                             '@clevercanyon/utilities.cfw',
+                                            '@clevercanyon/utilities.cfw/**',
+
                                             '@clevercanyon/utilities.cfp',
+                                            '@clevercanyon/utilities.cfp/**',
                                         ].filter((name) => name !== pkg.name && !name.startsWith(pkg.name + '/')),
                                     },
                                 }),
